6346198cb950e47efc149310271f34a310889a33
Liam Brewer <92010805+stevharve@users.noreply.github.com>
2022-03-26 23:36:12 -0500

    revert shop

--- src/App.js
+++ src/App.js
@@ -2,19 +2,10 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home';
 
-const Shop = () => {
-  return (
-    <div>
-      <h1>SHOP PAGE!!1</h1>
-    </div>
-  );
-};
-
 const App = () => {
   return (
     <Routes>
       <Route index element={<Home />} />
-      <Route path='shop' element={<Shop />} />
     </Routes>
   );
 };
--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -2,16 +2,11 @@ import './directory.scss';
 
 import CategoryItem from '../category-item/category-item';
 
-import { Link } from 'react-router-dom';
-
 const Directory = ({ categories }) => (
   <div className='directory-container'>
     {categories.map((category) => (
       <CategoryItem key={category.id} category={category} />
     ))}
-    <Link to='/shop'>
-      <h1>Shop</h1>
-    </Link>
   </div>
 );
 
