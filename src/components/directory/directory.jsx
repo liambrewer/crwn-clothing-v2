7530727bcb72140340ab8f370e26c6494a2a2154
Liam Brewer <92010805+stevharve@users.noreply.github.com>
2022-03-26 23:32:15 -0500

    Shop page test

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -2,11 +2,16 @@ import './directory.scss';
 
 import CategoryItem from '../category-item/category-item';
 
+import { Link } from 'react-router-dom';
+
 const Directory = ({ categories }) => (
   <div className='directory-container'>
     {categories.map((category) => (
       <CategoryItem key={category.id} category={category} />
     ))}
+    <Link to='/shop'>
+      <h1>Shop</h1>
+    </Link>
   </div>
 );
 
